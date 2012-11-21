db716f89a1b7c15d0d7d02c2f6e74d3c83c7a027
xiaocheng <xiaocheng@d0794262-6244-48d5-b1c3-ccf4b2f07ff9>
2012-11-21 01:28:46 +0000

    解决handle和applicationContext先后问题

--- branches/20121030/proxycache/src/main/java/com/taobao/pamirs/cache/framework/aop/handle/CacheManagerHandle.java
+++ branches/20121030/proxycache/src/main/java/com/taobao/pamirs/cache/framework/aop/handle/CacheManagerHandle.java
@@ -38,7 +38,7 @@ public class CacheManagerHandle extends AbstractAutoProxyCreator {
 		
 		if (ConfigUtil.isBeanHaveCache(cacheManager.getCacheConfig(), beanName)) {
 
-			log.info("CacheManager start... ProxyBean:" + beanName);
+			log.warn("CacheManager start... ProxyBean:" + beanName);
 
 			return new CacheManagerAdvisor[] { new CacheManagerAdvisor(
 					cacheManager, beanName) };

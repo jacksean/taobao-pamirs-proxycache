440a99efdc134dea9929f5d3a4b7b0e78f1e5f90
755935912 <755935912@d0794262-6244-48d5-b1c3-ccf4b2f07ff9>
2014-12-04 06:13:37 +0000

    兼容商品缓存双写

---
+++ proxycache/src/main/java/com/taobao/pamirs/cache/old/OldCacheManager.java
@@ -0,0 +1,104 @@
+package com.taobao.pamirs.cache.old;
+
+import java.io.Serializable;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.taobao.pamirs.cache.framework.config.MethodConfig;
+import com.taobao.tair.ResultCode;
+import com.taobao.tair.TairManager;
+
+/**
+ * 兼容写老的缓存，稳定后删除
+ * @author tiebi.hlw
+ *
+ */
+public class OldCacheManager {
+	
+	
+	private static final Log logger = LogFactory
+	.getLog(OldCacheManager.class);
+	
+	/**
+	 * 是否兼容
+	 */
+	private boolean isCompatible;
+
+	
+	private TairManager oldTairManager;
+	
+	
+	private String region;
+	
+	/**
+	 * 只作商品兼容
+	 */
+	private int namespace=318;
+	
+	
+	
+	public void put(String beanName,MethodInvocation invocation, Object value) {
+		
+		try{
+			String key=CacheUtils.getTairKey(region, invocation, beanName);
+			this.remove(key);
+			ResultCode rc = oldTairManager.put(namespace, key,
+					(Serializable) value, 0, 0);
+			if (!ResultCode.SUCCESS.equals(rc)) {
+				ResultCode rc1 = oldTairManager.put(namespace, key,
+						(Serializable) value, 0, 0);
+				if (!ResultCode.SUCCESS.equals(rc1)) {
+					logger.error("Tair Cache failed to invalid object [namespace="
+							+ namespace + ", key=" + key
+							+ "]. Error Message : " + rc1.getMessage());
+				}
+			}
+		}catch(Exception e){
+			logger.error("OldCacheManager put error,"+beanName+"."+invocation.getMethod().getName()
+					+" arg:"+invocation.getArguments()[0], e);
+		}
+	}
+	
+	
+	
+	public void remove(String beanName,MethodConfig method,Object[] params){
+		try{
+			String key=CacheUtils.getTairKey(region, beanName, method, params);
+			this.remove(key);
+		}catch(Exception e){
+			logger.error("OldCacheManager put error,"+beanName+"."+method.getMethodName()
+					+" arg:"+params[0], e);
+		}
+		
+	}
+	
+	private void remove(String key) {
+		ResultCode rc = oldTairManager.invalid(namespace, key);
+		if (!ResultCode.SUCCESS.equals(rc)) {
+			ResultCode rc1 = oldTairManager.invalid(namespace, key);
+			if (!ResultCode.SUCCESS.equals(rc)) {
+				logger.error("Tair Cache failed to invalid object [namespace="
+						+ namespace + ", key=" + key
+						+ "]. Error Message : " + rc1.getMessage());
+			}
+		}
+	}
+
+	public boolean isCompatible() {
+		return isCompatible;
+	}
+
+
+	public void setCompatible(boolean isCompatible) {
+		this.isCompatible = isCompatible;
+	}
+
+
+	public void setOldTairManager(TairManager oldTairManager) {
+		this.oldTairManager = oldTairManager;
+	}
+	
+
+}
